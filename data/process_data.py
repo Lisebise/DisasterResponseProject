d4378cf17da20e1ceb48e356c713d978db2e5a83
Lisebise <lisann.gerdes@web.de>
2022-12-21 16:12:03 +0100

    Add docstrings to the functions

--- data/process_data.py
+++ data/process_data.py
@@ -5,6 +5,15 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """Load the data from the csv files into a dataframe
+
+    :param messages_filepath: csv file path for the data of the messages
+    :type messages_filepath: str
+    :param categories_filepath: csv file path for the data of the categories
+    :type categories_filepath: str
+    :return: dataframe with the two inputs merged by id
+    :rtype: pd.DataFrame
+    """
     # load messages dataset
     messages = pd.read_csv(messages_filepath)
     # load categories dataset
@@ -14,6 +23,14 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+    """Clean the categories from the input, set appropriate column names, clean the categories classification and drop
+    duplicates
+
+    :param df: dataframe that holds the categories and messages
+    :type df: pd.DataFrame
+    :return: clean dataframe with an appropriate classification
+    :rtype: pd.DataFrame
+    """
     # create a dataframe of the 36 individual category columns
     categories = df["categories"].str.split(";", expand=True)
     # select the first row of the categories dataframe
@@ -28,6 +45,9 @@ def clean_data(df):
         # convert column from string to numeric
         categories[column] = pd.to_numeric(categories[column])
 
+    # changes the 2 in the related column to 1
+    categories['related'] = categories['related'].astype('str').str.replace('2', '1')
+    categories['related'] = categories['related'].astype('int')
     # drop the original categories column from `df`
     df.drop(columns=["categories"], inplace=True)
     # concatenate the original dataframe with the new `categories` dataframe
@@ -39,6 +59,13 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """Save the data to a sql database
+
+    :param df: dataframe that will be saved in the given database
+    :type df: pd.DataFrame
+    :param database_filename: name of the database with correct ending (.db)
+    :type database_filename: str
+    """
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('DisasterResponse', con=engine, if_exists='replace', index=False)
 
